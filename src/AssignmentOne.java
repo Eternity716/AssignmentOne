065bd0411c51ca90a94ff99c9d4f78bf717ced6a
Eternity716 <18277009985@163.com>
2024-11-18 11:54:17 +0800

    Part2 GeneralPractitioner Neurology

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -20,3 +20,39 @@ class HealthProfessional {
         System.out.println("Description: " + description);
     }
 }
+// "Part2:创建子类GeneralPractitioner";
+class GeneralPractitioner extends HealthProfessional {
+
+    public GeneralPractitioner() {
+    }
+
+    public GeneralPractitioner(int id, String name, String description) {
+        super(id, name, description);
+    }
+
+    // "打印详情printDetails";
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Specialty: HealthProfessional");
+    }
+}
+
+// "Part2:自定义Neurology";
+class Neurology extends HealthProfessional {
+    // "专业specialty";
+    String specialty = "Neurology";
+
+    public Neurology() {
+
+    }
+
+    public Neurology(int id, String name, String description) {
+        super(id, name, description);
+    }
+
+    // "打印函数printDetails";
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Specialty: " + this.specialty);
+    }
+}
